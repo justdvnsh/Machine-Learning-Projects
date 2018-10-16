96241ece190ecd6784b13104667325cd5e55b6f6
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-10-16 22:34:36 +0530

    ANN - added the prediction for the model and also predicted the churn rate for a particular customer

--- Deep-Learning/ANN/ann_py.py
+++ Deep-Learning/ANN/ann_py.py
@@ -24,7 +24,7 @@ X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -43,8 +43,46 @@ from keras.layers import Dense
 
 classifier = Sequential()
 
+## adding the input layer and first hidden layer
+
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+
+## adding the second layer
+
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+
+## adding the output layer
+
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+
+## compiling the ANN
+
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+## fitting the ANN to the training set.
+
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
+y_pred = ( y_pred > 0.5 )
+
+"""
+Prediction of the customer churn rate for this particular customer
+Geography: France
+Credit Score: 600
+Gender: Male
+Age: 40 years old
+Tenure: 3 years
+Balance: $60000
+Number of Products: 2
+Does this customer have a credit card ? Yes
+Is this customer an Active Member: Yes
+Estimated Salary: $50000
+"""
+
+new_pred = classifier.predict(sc.transform(np.array([[0.0, 0, 600, 1, 40, 3, 60000, 2, 1,1, 50000]])))
+new_pred = (new_pred > 0.5)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
