9f101b49cea8ea8d6f7ec08a0673946cfa6d34a9
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-10-18 10:04:11 +0530

    Classification
    Classification problems and their machine learning solutions

---
+++ Classification/logistic_regression/e_commerce.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  9 21:03:02 2018
+
+@author: Divyansh
+"""
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('./e_commerce.csv')
+
+print(df.head())
