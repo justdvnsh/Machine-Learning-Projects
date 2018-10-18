4152ba0407615e34c812edf6e9527c576619b04f
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-10-18 10:08:06 +0530

    CNN
    Deep Learning - CNN's

---
+++ Deep-Learning/Convolutional_Neural_Networks/cnn_py.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
