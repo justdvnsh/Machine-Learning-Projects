9687dee788f83c22eb5c87da966bdaec1233ce4c
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-10-18 18:35:47 +0530

    ann_py.py
    Updated ann model to improve its accuracy from 83 to 86%

--- Deep-Learning/ANN/ann_py.py
+++ Deep-Learning/ANN/ann_py.py
@@ -38,6 +38,7 @@ X_test = sc.transform(X_test)
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 
 # Create your classifier here
 
@@ -46,10 +47,12 @@ classifier = Sequential()
 ## adding the input layer and first hidden layer
 
 classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+classifier.add(Dropout(p = 0.1))
 
 ## adding the second layer
 
 classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+classifier.add(Dropout(p = 0.1))
 
 ## adding the output layer
 
@@ -87,3 +90,83 @@ new_pred = (new_pred > 0.5)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+## Now , since we get different acccuracies when we train models different times, we are facing the Bias-Variance tradeoff.
+## So , in order to avoid that we need to apply the K-Fold Cross Validation method, which is nothing, but , it 
+## divides the training model into separate training folds and test the model, on the same exact fold.
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+
+def build_classifier():
+    
+    classifier = Sequential()
+
+    ## adding the input layer and first hidden layer
+
+    classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+
+    ## adding the second layer
+
+    classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+
+    ## adding the output layer
+
+    classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+
+    ## compiling the ANN
+
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+    return classifier
+
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, nb_epoch = 100)
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = -1)
+mean = accuracies.mean()
+variance = accuracies.std()
+
+print('Mean Accuracy: {0} and Mean Variance: {1}'.format(mean, variance))
+
+## Tuning the ANN
+
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+
+def build_classifier(optimizer):
+    
+    classifier = Sequential()
+
+    ## adding the input layer and first hidden layer
+
+    classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+
+    ## adding the second layer
+
+    classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+
+    ## adding the output layer
+
+    classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+
+    ## compiling the ANN
+
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+    return classifier
+
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, nb_epoch = 100)
+paramters = {
+    'batch_size': [25, 35, 50],
+    'nb_epoch': [200, 500, 800],
+    'optimizer': ['adam', 'rmsprop']
+}
+
+grid_search = GridSearchCV(estimator = classifier, param_grid = paramters, scoring = 'accuracy', cv = 10)
+
+grid_search = grid_search.fit(X_train, y_train)
+
+best_paramters = grid_search._best_params_
+best_accuracy = grid_search._best_score_
+
+print('Best Paramters are: {0} and Best Accuracy is: {1} '.format(best_paramters, best_accuracy))
